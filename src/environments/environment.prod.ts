f4e8ec842bbc1aa9c7159a3efce757803f12447f
Juan Manuel Tarrago <81875517+juanma414@users.noreply.github.com>
2026-03-05 18:20:09 -0300

    Update production API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api' // Cambiar por la URL de producción cuando sea necesario
+  apiUrl: 'https://handsome-integrity-production.up.railway.app/api' // Cambiar por la URL de producción cuando sea necesario
 };
